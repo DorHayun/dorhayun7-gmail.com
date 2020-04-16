3ef1867e40f992cf1a9ed7596216ef07d6b25c22
Dor Hayun <39770712+DorHayun@users.noreply.github.com>
2020-04-16 22:37:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def hello_world():
     license = soup.findAll('p' ,attrs={"class" :"f2874b88 fw6 mb3 mt2 truncate black-80 f4"})[1].text
     #send msg in the telegram bot
     requests.get("https://api.telegram.org/bot857617376:AAF_rlduJrIP1iXfPDRj84ixm3JOzewjouw/sendMessage?chat_id=478322885&text={}".format(license))
-    print('the data was sent!')
-    return
+    return'the data was sent!'
+    
     
