f2d4e078deca15f5fee4efa2d4db3951978ad1fc
Dor Hayun <39770712+DorHayun@users.noreply.github.com>
2020-04-16 22:15:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ app = Flask(__name__)
 
 @app.route('/home')
 def hello_world():
-    return 'Hello, World!'
-
-result = requests.get("https://www.npmjs.com/package/lodash")
-
-src = result.content
-soup = BeautifulSoup(src, 'html.parser')
-License = soup.findAll('p' ,attrs={"class" :"f2874b88 fw6 mb3 mt2 truncate black-80 f4"})[1].text
-
-#send msg in the telegram bot
-requests.get("https://api.telegram.org/bot857617376:AAFktPZYQfRG60voVoWvPyO7eFKWUahmEKE/sendMessage?chat_id=478322885&text={}".format(License))
+    result = requests.get("https://www.npmjs.com/package/lodash")
+    src = result.content
+    soup = BeautifulSoup(src, 'html.parser')
+    license = soup.findAll('p' ,attrs={"class" :"f2874b88 fw6 mb3 mt2 truncate black-80 f4"})[1].text
+    #send msg in the telegram bot
+    requests.get("https://api.telegram.org/bot857617376:AAFktPZYQfRG60voVoWvPyO7eFKWUahmEKE/sendMessage?chat_id=478322885&text={}".format(License))
+    return 'the data was sent!'
