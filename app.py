9073284b4edc5d8b416320a39bbcad869d61280c
Dor Hayun <39770712+DorHayun@users.noreply.github.com>
2020-05-19 14:37:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,25 @@ from unshortenit import UnshortenIt
 app = Flask(__name__)
 
 @app.route('/')
+
+
+def main():
+ unshortener = UnshortenIt() 
+ 
+ while True:
+  updates = requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/getUpdates")
+  offset = updates[0]["update_id"]
+  for item in updates:
+   
+   updates = requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/getUpdates?offset={}".format(offset+1))
+   updates = json.loads(updates.content)
+   updates = updates["result"]                  
+   message = item["message"]["text"]
+   message = unshortener.unshorten(message)
+   requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/sendMessage?chat_id=478322885&text={}".format(message))
+   return message
+    
+
 #def hello_world():
     #result = requests.get("https://www.npmjs.com/package/lodash")
    # src = result.content
@@ -19,21 +38,3 @@ app = Flask(__name__)
     #return'the data was sent!'#
      
    #get the url from user
-
-def main():
- unshortener = UnshortenIt() 
- 
- while True:
- 
-  for item in updates:
-   offset = item["update_id"]
-   updates = requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/getUpdates?offset={}".format(offset))
-   offset = offset+1
-   updates = json.loads(updates.content)
-   updates = updates["result"]                  
-   message = item["message"]["text"]
-   message = unshortener.unshorten(message)
-   requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/sendMessage?chat_id=478322885&text={}".format(message))
-   return message
-    
-main()
